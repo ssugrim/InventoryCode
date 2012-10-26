7721c9bab07b5ede6fd2b02bbb0b6bfe68b72733
Shridatt Sugrim <ssugrim@winlab.rutgers.edu>
2012-10-26 14:59:19 -0400

    working viewer that oputputs html

--- viewer.rb
+++ viewer.rb
@@ -4,31 +4,7 @@
 require 'optparse'
 require 'singleton'
 require 'log_wrap'
-require 'rest_db'
-
-class WebNodeData
-	#container class for all the data and meta data extraced from the DB
-	def initialize(fqdn)
-		@log  = LOG.instance
-		
-		#all the data fromt he restDB for this given fqdn
-		db = Database.new($options[:dbserver])
-		@nodes = db.get_all_node(fqdn)
-
-		#all the possible headers expect name
-		@headers = @nodes.inject(Array.new){|s,c| s.push(c.map{|x| x.first})}.flatten.uniq.reject{|x| x == "name"}.sort
-
-		#all the names
-		@names = @nodes.flatten.join(" ").scan(/(node\d+-\d+\.#{fqdn})/).flatten
-	end
-
-	def get_node_data(name)
-		#name is a string, the name of the node whose data we want
-		return Tools.tuples(@nodes.select{|x| Tools.contains?(name,x)})
-	end
-
-	attr_reader :nodes, :headers, :names
-end
+require 'webnodedata'
 
 class Local_host_name
 	include Singleton
@@ -100,7 +76,7 @@ if __FILE__ == $0
 
 	begin
 		lhn = Local_host_name.instance
-		data = WebNodeData.new(lhn.fqdn)
+		data = WebNodeData.new($options[:dbserver], lhn.fqdn)
 		print "<HTML><BODY><TABLE BORDER = 1><TR><TD>Name</TD><TD>",data.headers.join("</TD><TD>"),"</TD></TR>\n"
 		data.names.each{|x| 
 			print "<TR><TD>#{x}</TD><TD>";
