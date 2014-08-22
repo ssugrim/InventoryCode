be8bac82e8d0b43a64a18b4efcc98af6e9a611cb
Shridatt Sugrim <ssugrim@winlab.rutgers.edu>
2014-08-22 17:40:21 -0400

    Minor updates to rest_db and inventory for compliance with new ruby version

--- inventory.sh
+++ inventory.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "Sleeping 30" > /tmp/inventory.log
+echo "Waiting for network interfaces to come up, Sleeping 30" > /tmp/inventory.log
 sleep 30
 let amt=$RANDOM%10 
 echo "Sleep $amt" >> /tmp/inventory.log
 sleep $amt 
 echo "NTP info" >> /tmp/inventory.log
-ntpq --numeric --peers >> /tmp/inventory.log
+/usr/sbin/ntpd -d -n -q -p consolec
+echo "Fixing HW clock" >> /tmp/inventory.log
+/sbin/hwclock --directisa -w
+/sbin/hwclock >> /tmp/inventory.log
 echo "Starting gatherer" >> /tmp/inventory.log
 ruby -I /root/ /root/gatherer.rb -d  -l /tmp/gatherer.log >> /tmp/inventory.log
 echo "Done" >> /tmp/inventory.log
-
